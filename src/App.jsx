c2e2680371033a2eb16733277ff46460dae251e5
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-08-22 08:14:27 +0300

    file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold text-red-800 underline">
-      Hello world!
-    </h1>
+import { Hero, PopularProducts, SuperQuality, Services, SpecialOffer, CustomerReviews, Subscribe, Footer} from './sections'
+import Nav from './components/Nav'
+const App = () => {
+  return(
+    <main className="relative">
+      <Nav/>
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero/>
+      </section>
+      <section className="padding">
+        <PopularProducts/>
+      </section>
+      <section className="padding ">
+        <SuperQuality/>
+      </section>
+      <section className="padding padding-x py-10">
+        <Services/>
+      </section>
+      <section className="bg-pale-blue padding">
+        <SpecialOffer/>
+      </section>
+      <section className="padding">
+        <CustomerReviews/>
+      </section>
+      <section className="padding-x sm:py-16 py-16 w-full">
+        <Subscribe/> 
+      </section>
+      <section className="bg-black padding-x padding-t pb-8">
+        <Footer/> 
+      </section>
+    </main>
   )
 }
+
+export default App
