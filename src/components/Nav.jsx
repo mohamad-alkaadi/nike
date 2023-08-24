5580effc3c0372f9bc335a3e809baac4b91272d4
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-08-24 21:21:34 +0300

    finished navbar without logic

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {hamburger} from "../assets/icons/index"
 import {headerLogo} from "../assets/images//index"
+import { navLinks } from '../constants'
 const Nav = () => {
   return (
     <header className='padding-x py-8 absolute z-10 w-full'>
@@ -9,8 +10,15 @@ const Nav = () => {
           <img src={headerLogo} alt='Logo' width={130} height={29}/>
         </a>
         <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
-          <li></li>
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a href={item.href} className='font-montserrat landing-normal text-lg text-slate-gray'>{item.label}</a>
+            </li>
+          ))}
         </ul>
+        <div className='lg:hidden'>
+          <img src={hamburger} alt='Hamburger' width={25} height={25}/>
+        </div>
       </nav>
     </header>
   )
