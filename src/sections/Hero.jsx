d9cc151bb60170712fe2ee84fe425c881d264ff2
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-08-25 22:19:10 +0100

    hero structure

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section id="home" className='w-full p-2 flex flex-col xl:flex-row justify-center min-h-screen gap-10 max-container'>
+      Hero
+    </section>
   )
 }
 
