4a516263a404cc8e986072cca94c688be76d43f8
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-08-26 23:23:25 +0300

    update too hero and new button style

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import Button from '../components/Button'
+import {arrowRight} from '../assets/icons/index'
 const Hero = () => {
   return (
     <section id="home" className='w-full p-2 flex flex-col xl:flex-row justify-center min-h-screen gap-10 max-container'>
-      Hero
+      <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28'>
+        <p className=''>Our Summer Collection</p>
+        <h1 className=''>
+          <span>The New Arrival</span>   
+          <br/>
+          <span className=''>Nike</span>
+          Shoes
+        </h1>
+        <p>Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
+        <Button label="Shop now" iconURL={arrowRight}/>
+      </div>
     </section>
   )
 }
