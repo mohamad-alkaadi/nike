cf1b6613b2f47c64dd06e7facb895c106bb84eb6
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-09-05 23:55:38 +0300

    special offer and update to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-const Button = ({label, iconURL}) => {
+const Button = ({label, iconURL, backgroundColor, borderColor, textColor}) => {
   return (
-    <button className='flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none bg-coral-red text-white border border-coral-red rounded-full'>
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none rounded-full
+     ${backgroundColor? `${backgroundColor} ${textColor} ${borderColor}`: `bg-coral-red text-white border border-coral-red`}`}>
         {label}
         {iconURL &&
           <img src={iconURL} alt='arrow right icon' className='ml-2 rounded-full w-5 h-5'/>
--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { offer } from '../assets/images'
 import Button from '../components/Button'
+import {arrowRight} from '../assets/icons'
 
 const SpecialOffer = () => {
   return (
@@ -14,8 +15,9 @@ const SpecialOffer = () => {
           </h2>
           <p className='mt-4 lg:max-w-lg info-text'>Embark on a shopping journey that redefines your experience with unbeatable deals. From premier selections to incredible savings, we offer unparalleled value that sets us apart.</p>
           <p className='mt-4 lg:max-w-lg info-text'>Navigate a realm of possibilities designed to fulfill your unique desires, surpassing the loftiest expectations. Your journey with us is nothing short of exceptional.</p>
-          <div className='mt-11'>
-            <Button label="View details" />
+          <div className='mt-11 flex flex-wrap gap-4'>
+            <Button label="View details" iconURL={arrowRight}/>
+            <Button label="learn more" backgroundColor="bg-white" borderColor="border-slate-gray" textColor="text-slate-gray" iconURL={arrowRight} />
           </div>
         </div>
     </section>
