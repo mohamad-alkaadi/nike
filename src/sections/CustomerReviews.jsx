4f1f9b70d7e964381a2b330b9ae3f0c968e353d3
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-09-09 23:45:18 +0300

    starting customer reviews

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
+    <section>
+      <h3 className='font-palanquin text-center text-4xl font-bold'>What Our <span className='text-coral-red'>Customers</span> Say?</h3>
+      <p className='info-text m-auto mt-4 max-w-lg text-center'>
+        Hear genuine stories from our satisfied customers about their exceptional experiences with us.
+      </p>
+    </section>
   )
 }
 
