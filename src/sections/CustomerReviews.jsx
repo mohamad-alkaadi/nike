f08fb0c6a806972801de7e919dc2f14eb77cdab2
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-09-13 20:16:51 -0300

    starting review card

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { reviews } from '../constants'
 
 const CustomerReviews = () => {
   return (
@@ -7,6 +8,11 @@ const CustomerReviews = () => {
       <p className='info-text m-auto mt-4 max-w-lg text-center'>
         Hear genuine stories from our satisfied customers about their exceptional experiences with us.
       </p>
+      <div className='mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14'>
+        {reviews.map((review) => {
+          <Review/>
+        })}
+      </div>
     </section>
   )
 }
