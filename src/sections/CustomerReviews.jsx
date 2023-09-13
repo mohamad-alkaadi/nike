11b228165b9bad73010d29da2dbd84c295727294
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-09-13 20:17:14 -0300

    review card

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { reviews } from '../constants'
+import ReviewCard from '../components/ReviewCard'
 
 const CustomerReviews = () => {
   return (
@@ -10,7 +11,9 @@ const CustomerReviews = () => {
       </p>
       <div className='mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14'>
         {reviews.map((review) => {
-          <Review/>
+          <ReviewCard key={review.customerName} imgURL={review.imgURL}
+            customerName={review.customerName} rating={review.rating}
+            feedback={review.feedback}/>
         })}
       </div>
     </section>
