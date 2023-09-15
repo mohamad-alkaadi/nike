7923954b80d0bf7c5b373c9f073653596032eecd
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-09-14 21:15:19 -0400

    finished subscribe

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Button = ({label, iconURL, backgroundColor, borderColor, textColor}) => {
+const Button = ({label, iconURL, backgroundColor, borderColor, textColor, fullWidth}) => {
   return (
-    <button className={`flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none rounded-full
-     ${backgroundColor? `${backgroundColor} ${textColor} ${borderColor}`: `bg-coral-red text-white border border-coral-red`}`}>
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+     ${backgroundColor? `${backgroundColor} ${textColor} ${borderColor}`: `bg-coral-red text-white border-coral-red`} rounded-full first-letter ${fullWidth && 'w-full'}`}>
         {label}
         {iconURL &&
           <img src={iconURL} alt='arrow right icon' className='ml-2 rounded-full w-5 h-5'/>
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -7,10 +7,10 @@ const Subscribe = () => {
         Sign Up from <span className='text-coral-red'>Updates</span> & 
         Newsletter
       </h3>
-      <div>
+      <div className='lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full'>
         <input type='text' placeholder='subscribe@nike.com' className='input'/>
-        <div>
-          <Button label="Sign Up" fullWid/>
+        <div className='flex max-sm:justify-end items-center max-sm:w-full'>
+          <Button label="Sign Up" fullWidth/>
         </div>
       </div>
     </section>
