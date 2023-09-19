b81805191d2857fcba81e2b7db23fc6ffa84ef9d
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-09-18 19:25:40 -1200

    update to popular products

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -6,11 +6,7 @@ const PopularProducts = () => {
   return (
     <section id="products" className='max-container max-sm:mt-12'>
         <div className='flex flex-1 flex-col justify-start gap-5'>
-          <div className='flex justify-between'>
-            <h2 className='text-4xl font-palanquin font-bold'>Our <span className='text-coral-red'>Popular</span> Products</h2>
-            {/* <a className='grid justify-center items-center border border-2 pr-4 pl-4 border-coral-red rounded-lg'><p className='text-lg'>All products</p></a> */}
-            <Button label="All products"/>
-          </div>
+          <h2 className='text-4xl font-palanquin font-bold'>Our <span className='text-coral-red'>Popular</span> Products</h2>
           <p className='lg:max-w-lg mt-2 font-montserrat text-slate-gray'>
             Experience top-notch quality and style with our sought-after selections.
             Discover a world of comfort, design, and value
