e1c7cd3f23ec8b643375134c5efa6e73c5c6d62a
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-09-20 20:12:43 -1200

    update to nav and pictures

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import {hamburger} from "../assets/icons/index"
 import {headerLogo} from "../assets/images//index"
 import { navLinks } from '../constants'
+import { GrClose, GrMenu } from 'react-icons/gr';
 
 const Nav = () => {
   const [nav, setNav] = useState(false)
@@ -9,18 +10,19 @@ const Nav = () => {
     <header className='padding-x py-8 absolute z-10 w-full'>
       <nav className='flex justify-between items-center max-container'>
         <a href='/'>
-          <img src={headerLogo} alt='Logo' width={130} height={29}/>
+          <img src={headerLogo} className={`${nav && 'hidden'}`} alt='Logo' width={130} height={29}/>
         </a>
-        <ul className={`flex-1 flex justify-center items-center gap-16 max-lg:flex-col max-lg:h-fit max-lg:w-full max-lg:absolute max-lg:left-0 max-lg:bg-green-100 max-lg:pt-40 max-lg:gap-1 max-lg:items-center max-lg:opacity-0`}>
+        <ul className={`flex-1 flex justify-center items-center gap-16 max-lg:flex-col max-lg:h-fit max-lg:w-full max-lg:absolute max-lg:left-0 max-lg:bg-coral-red max-lg:pt-40 max-lg:gap-1 max-lg:items-center ${!nav && 'max-lg:opacity-0'}`}>
           {navLinks.map((item) => (
             <li key={item.label} className='max-lg:pb-6'>
-              <a href={item.href} className='font-montserrat landing-normal text-lg text-slate-gray'>{item.label}</a>
+              <a href={item.href} className='font-montserrat landing-normal text-lg text-slate-gray max-lg:text-white'>{item.label}</a>
             </li>
           ))}
         </ul>
-        <div className='lg:hidden'>
-          <img src={hamburger} alt='Hamburger' width={25} height={25}/>
-        </div>
+        <button className='lg:hidden cursor-pointer' onClick={() => setNav(!nav)}>
+          <GrMenu className={`w-[25px] h-[25px] absolute cursor-pointer ${nav && 'opacity-0'}`}/>
+          <GrClose className={`w-[25px] h-[25px]  ${!nav && 'opacity-0'} ${nav && 'opacity-1 relative'}`}/>
+        </button>
       </nav>
     </header>
   )
