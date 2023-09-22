70813c972f4db374552ceb7442e83b6281a9f067
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-09-21 23:56:34 -1200

    update to nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,7 +10,8 @@ const Nav = () => {
     <header className={`padding-x py-8 absolute z-10 w-full ${nav && 'max-lg:bg-slate-400'}`}>
       <nav className={`flex justify-between items-center max-container`}>
         <a href='/'>
-          <img src={headerLogo} alt='Logo' width={130} height={29}/>
+          <img src={headerLogo} alt='Logo' 
+            width={130} height={29}/>
         </a>
         <ul className={`flex-1 flex justify-center items-center gap-16 max-lg:hidden`}>
           {navLinks.map((item) => (
